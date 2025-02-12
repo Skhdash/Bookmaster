a3475bb3b1cd003c400ae67473c76fe475b07c31
схведиани.дарья <darya.shvediani@yandex.ru>
2025-02-12 12:39:04 +0300

    Добавлены и подключены словари ресурсов.

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bookmaster.View.Pages;
+using Bookmaster.View.Windows;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bookmaster
 {
@@ -23,6 +12,52 @@ namespace Bookmaster
         public MainWindow()
         {
             InitializeComponent();
+            LogoutMi.Visibility = Visibility.Collapsed;
+            LibraryMi.Visibility = Visibility.Collapsed;
+        }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            //Для реализации оконной навигации нужно:
+            // 1) Создать экземпляр окна, котрое требуется открыть
+            LoginWindow loginWindow = new LoginWindow();
+
+            // 2) У экземпляра окна вызвать метод Show() или ShowDialog();
+            loginWindow.ShowDialog();
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BrowseCatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+            // Для реализации страничной навигации нужно:
+            // 1) Обратиться к элементу Frame по имени и вызываем метод Navigate()
+            // 2) В качестве аргумента передаем в метод экземпляр страницы, которую нужно открыть.
+            MainFrame.Navigate(new BrowseCatalogPage());
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ManageCustomersPage());
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ReportsPage());
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CirculationPage());
         }
     }
 }
